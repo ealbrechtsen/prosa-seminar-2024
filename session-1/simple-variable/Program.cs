4b71b55de73aff9807731123aba5d214c9d977e8
ealbrechtsen <emilmalikalbrechtsen@gmail.com>
2024-09-15 22:00:48 +0200

    session 1 finished

---
+++ session-1/simple-variable/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace simple_variable;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // erklære variabel og tildel værdi
+        int variable = 10;
+        // anvende operatorer
+        variable++;
+        Console.WriteLine(variable);
+
+        variable--;
+        Console.WriteLine(variable);
+
+        variable += 20;
+        Console.WriteLine(variable);
+
+        // erklære double decimal
+        double decimated = 12.5;
+
+        Console.WriteLine(decimated);
+
+        decimated++;
+        Console.WriteLine(decimated);
+
+        decimated--;
+        Console.WriteLine(decimated);
+
+        decimated *= 2;
+        Console.WriteLine(decimated);
+        
+
+    }
+}
