fd863ada8cb7355cece1565f2b70781c4887e2f2
ealbrechtsen <emilmalikalbrechtsen@gmail.com>
2024-09-16 23:06:04 +0200

    usage of substring

---
+++ session-1/1-use-of-strings/1-use-of-strings/1-use-of-strings/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _1_use_of_strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Mikkel";
+            string lastName = "Cronberg";
+            string nameString = firstName + lastName;
+            Console.WriteLine($"{nameString.ToUpper()}");
+        }
+    }
+};
