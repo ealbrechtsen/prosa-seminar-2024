464f155992454c25554a062518e001b3880c6119
ealbrechtsen <emilmalikalbrechtsen@gmail.com>
2024-09-16 23:06:04 +0200

    corrected outputusage of substring

---
+++ session-1/1-use-of-strings/1-use-of-strings/1-use-of-strings/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _1_use_of_strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Mikkel";
+            string lastName = "Cronberg";
+            string nameString = firstName + " " +lastName;
+            
+            string uppercaseName = nameString.ToUpper();
+            Console.WriteLine(uppercaseName);
+
+            string lowercaseName = nameString.ToLower();
+            Console.WriteLine(lowercaseName);
+
+            string subString = nameString.Substring(7, 4);
+            Console.WriteLine(subString);
+            Console.WriteLine(firstName + "\r\n\t" + lastName);
+        }
+    }
+};
