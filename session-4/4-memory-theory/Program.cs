7770b790aa00cb3f19dac91c78bbd82d30c38c18
Emil Albrechtsen <145019248+ealbrechtsen@users.noreply.github.com>
2024-10-12 23:04:35 +0200

    N215 Hukommelsesteori 1

---
+++ session-4/4-memory-theory/Program.cs
@@ -0,0 +1,23 @@
+﻿using _4_memory_theory;
+using System;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Person k1 = new Person();
+        k1.Id = 1;
+        Person k2 = new Person();
+        k2.Id = 2;
+        Console.WriteLine(k1.Id);
+        Console.WriteLine(k2.Id);
+        k1 = k2;
+        Console.WriteLine(k1.Id);
+        Console.WriteLine(k2.Id);
+        k1.Id = 3;
+        Console.WriteLine(k1.Id);
+        Console.WriteLine(k2.Id);
+    }
+}
