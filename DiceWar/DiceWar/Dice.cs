2d265308a34dbd33b1f413ac792e1a83063900b2
Emil Albrechtsen <145019248+ealbrechtsen@users.noreply.github.com>
2024-11-06 23:19:40 +0100

    Finished step 1 in DiceWar game

---
+++ DiceWar/DiceWar/Dice.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Pcg;
+
+namespace DiceWar
+{
+    public class Dice
+    {
+        public int Value { get; private set; }
+
+        private static readonly Random rng = new PcgRandom();
+
+
+        // Sætter Value til et tilfældigt tal mellem 1 og 6 
+        public void Roll ()
+        {
+            Value = rng.Next(1, 7);
+        }
+
+        // Returnerer værdien af Value
+        public virtual int Point()
+        {
+            return Value;
+        }
+
+        // Viser værdien som string
+        public override string ToString()
+        {
+            return $"[{Value}]";
+        }
+    }
+}
---
+++ DiceWar/DiceWar/WarDice.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceWar
+{
+    public class WarDice : Dice
+    {
+        public override int Point()
+        {
+            if (Value % 2 == 1)
+            {
+                return Value * 2;
+            }
+            else
+            {
+                return Value;
+            }
+        }
+
+    }
+}
